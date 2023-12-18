e81dc8677ec76324a73e829d60980e1de04adcc0
لطفی نیا،مدیرتیم پشتیبانی سامانه ها <E.Lotfi@exir.rayaneh>
2023-12-18 17:50:49 +0330

    init

---
+++ friends/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'friends',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
