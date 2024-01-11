5d85242e4613a685b9ccc507ff87349b74c8cffe
Ehsan Lotfinia <lotfiniamail@gmail.com>
2024-01-11 11:54:46 +0330

    Update vite.config.ts

--- friends-react/vite.config.ts
+++ friends-react/vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+    port: 4200,
+  },
   plugins: [
     react(),
     legacy()
