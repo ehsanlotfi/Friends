fb14a08642328185c9e2b74929f1fa5d20e5d03e
Ehsan Lotfinia <lotfiniamail@gmail.com>
2024-01-11 13:45:15 +0330

    fix

--- friends/src/app/app-routing.module.ts
+++ friends/src/app/app-routing.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import * as _pages from 'src/app/pages';
+
 
 const routes: Routes = [
+  { path: '', component: _pages.SplashComponent },
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    path: 'app', component: _pages.LayoutComponent, children: [
+      { path: '', component: _pages.SeasonsComponent },
+      { path: ':seasonId/detail', component: _pages.SeasonDetailsComponent },
+      { path: ':seasonId/episods', component: _pages.EpisodesComponent },
+      { path: ':seasonId/episods/:episodeId', component: _pages.QuotesComponent }
+    ]
+  },
+  {
+    path: 'leitner', component: _pages.LayoutComponent, children: [
+      { path: '', component: _pages.LeitnerListComponent },
+    ]
+  },
+  {
+    path: 'about', component: _pages.LayoutComponent, children: [
+      { path: '', component: _pages.AboutUsComponent },
+    ]
+  },
 ];
+
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
---
+++ friends/src/app/pages/seasons/seasons.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import * as _svc from 'src/app/services';
+import * as _mod from 'src/app/models';
+
+@Component({
+  selector: 'app-seasons',
+  templateUrl: './seasons.component.html',
+  styleUrls: ['./seasons.component.scss']
+})
+export class SeasonsComponent implements OnInit {
+
+  seasons: _mod.Season[] = [];
+
+  constructor(private globalService: _svc.GlobalService,) { }
+
+  ngOnInit(): void {
+    this.seasons = this.globalService.getAllSeasons();
+  }
+
+}
