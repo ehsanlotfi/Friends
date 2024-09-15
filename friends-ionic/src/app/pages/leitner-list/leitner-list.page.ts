ac294c2a68e1423361fb1e5c650aa8ce824d3409
لطفی نیا،مدیرتیم پشتیبانی سامانه ها <E.Lotfi@exir.rayaneh>
2024-09-15 14:06:51 +0330

    fix

--- friends-ionic/src/app/pages/leitner-list/leitner-list.page.ts
+++ friends-ionic/src/app/pages/leitner-list/leitner-list.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as _svc from 'src/app/services';
+import * as _mod from 'src/app/models';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-leitner-list',
   templateUrl: 'leitner-list.page.html'
 })
-export class LeitnerListPage
+export class LeitnerListPage implements OnInit
 {
-  isFlipped = false;
-  constructor() { }
+
+  quotes: _mod.Quote[] = [];
+  quote?: _mod.Quote;
+
+  type = _mod.QuoteType;
+
+  constructor(
+    private globalService: _svc.GlobalService) { }
+
+  ngOnInit(): void
+  {
+    this.getData();
+  }
+
+  getData()
+  {
+    this.globalService.getCards().subscribe(data =>
+    {
+      this.quotes = data;
+      if (this.quotes && this.quotes.length)
+      {
+        this.quote = this.quotes[0];
+      }
+    });
+  }
+
+  setLeitnerType(id: number, type: _mod.QuoteType)
+  {
+    const index = this.quotes.findIndex(f => f.ID === id);
+    this.globalService.setLeitnerType(id, type).subscribe(res =>
+    {
+      this.quote = this.quotes[index + 1];
+    });
+  }
 
 }
